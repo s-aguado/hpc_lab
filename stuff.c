3aa354b94ff06afcdbfce30faa14ad2ddc7a5440
Emilio J. Padrón González <emilioj@udc.gal>
2018-05-21 10:49:39 +0200

    Auxiliary stuff: initializing, measuring time...

---
+++ stuff.c
@@ -0,0 +1,54 @@
+#include "stuff.h"
+
+void get_walltime(const resnfo start, const resnfo end, timenfo *const t)
+{
+#ifdef _noWALL_
+  t->time = (end.ru_utime.tv_sec + (end.ru_utime.tv_usec * 1E-6))
+    - (start.ru_utime.tv_sec + (start.ru_utime.tv_usec * 1E-6));
+  t->systime = (end.ru_stime.tv_sec + (end.ru_stime.tv_usec * 1E-6))
+    - (start.ru_stime.tv_sec + (start.ru_stime.tv_usec * 1E-6));
+#else
+  *t = (end.tv_sec + (end.tv_usec * 1E-6))
+    - (start.tv_sec + (start.tv_usec * 1E-6));
+#endif /*_noWALL_*/
+}
+
+void populating_arrays(basetype arrayA[], basetype arrayB[],
+                       basetype arrayR[], const unsigned int n)
+{
+  unsigned int i;
+
+  srand(time(NULL));
+
+  for(i = 0; i < n; i++) {
+#ifdef _INT_
+    arrayA[i] = rand();
+    arrayB[i] = rand();
+    arrayR[i] = 0;
+#else
+    arrayA[i] = rand() / (RAND_MAX + 1.);
+    arrayB[i] = rand() / (RAND_MAX + 1.);
+    arrayR[i] = 0.0;
+#endif
+  }
+}
+
+
+basetype check_result_and_reset(basetype array[], const unsigned int n)
+{
+  unsigned int i;
+  basetype result = 0;
+
+  for(i = 0; i < n; i++) {
+    result += array[i];
+    array[i] = (basetype) 0;
+  }
+
+  return(result);
+}
+
+
+void reset_matrix(basetype array[], const unsigned int n)
+{
+  memset(array, 0, n);
+}
